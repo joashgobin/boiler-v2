b2594073ea20b34acfe2be6e5fafcbd976158a0b
joashgobin <joashgobin@gmail.com>
2026-09-20 12:30:29 -0400

    Added functions for saving structs to and loading struct from files

--- helpers/files.go
+++ helpers/files.go
@@ -1,12 +1,15 @@
 package helpers
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"strings"
 	"time"
 
-	"github.com/gofiber/fiber/v3"
 	"uuid"
+
+	"github.com/gofiber/fiber/v3"
 )
 
 type FilesInterface interface {
@@ -32,4 +35,30 @@ func (files *FilesModel) UploadImage(c fiber.Ctx, imageFormField string) (string
 		return "", err
 	}
 	return image, nil
+}
+
+func SaveStructToFile[T any](item T, fileName string) error {
+	data, err := json.MarshalIndent(item, "", "  ")
+	if err != nil {
+		return fmt.Errorf("save struct to file marshall error: %v", err)
+	}
+	err = os.WriteFile(fileName, data, 0644)
+	if err != nil {
+		return fmt.Errorf("save struct to file write error: %v", err)
+	}
+	return nil
+}
+
+func LoadStructFromFile[T any](structExample T, fileName string) (T, error) {
+	var defaultValue T
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		return defaultValue, fmt.Errorf("load struct from file read error: %v", err)
+	}
+	var value T
+	err = json.Unmarshal(data, &value)
+	if err != nil {
+		return defaultValue, fmt.Errorf("load struct from file unmarshal error: %v", err)
+	}
+	return value, nil
 }
---
+++ logs/joashgobin-2026-09-20.md
@@ -0,0 +1,3 @@
+# Sunday (September 20, 2026)
+
+- Added functions for saving structs to and loading struct from files
